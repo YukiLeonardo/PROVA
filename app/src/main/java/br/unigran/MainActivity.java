89a98ceec99736811a72e91c8a8b044a81b9531b
YukiLeonardo <802.293@alunos.unigran.br>
2022-09-23 20:02:26 -0400

    Tela Grafica

--- app/src/main/java/br/unigran/MainActivity.java
+++ app/src/main/java/br/unigran/MainActivity.java
@@ -3,6 +3,7 @@ package br.unigran;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
